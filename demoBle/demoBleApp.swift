fb03a1cb22ad4d6927c45acda92d5a4af7d667ca
HoDoan <doanphj5@gmail.com>
2023-04-03 15:43:10 +0700

    Initial Commit

---
+++ demoBle/demoBleApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoBleApp.swift
+//  demoBle
+//
+//  Created by Doan Ho on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct demoBleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
